2d724c04ea03e8dc96ff6b8e4449dd6de87ed672
jaydeegbobeh <jaydeegbobeh@gmail.com>
2021-06-30 15:37:11 +0100

    deletion_test

--- test_cases/test_account_deletion.py
+++ test_cases/test_account_deletion.py
@@ -8,13 +8,13 @@ import random
 
 from Password_Analyzer import PasswordChecker
 
-
+# use mock  package to run tests
 class PassTest(unittest.TestCase):
         checker = AccountChecker()
         manage = AccountManager()
     def test_delete_account(self):
         self.assertTrue(self.table_update(self, "something"))
-    #check if the database has been updated?
+    #check if the database has been updat
     def test_check_deletion(self):
         self.assertFalse(self.check_existence(self, "username"))
     #check if the account no longer exists
