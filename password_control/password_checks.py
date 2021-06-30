ea097a05b5bbd4fd340eab440ffdf57f2df67965
kaiwolff <kai.wolff.92@gmail.com>
2021-06-30 12:21:57 +0100

    added read_password_policy and password policy

--- password_control/password_checks.py
+++ password_control/password_checks.py
@@ -1,75 +1,91 @@
-def check_list(self, password):
-    # checks password against passwords in common_passwords.txt. Returns True if password is not in file, False if found.Written by KW
-    # sql_password = getpass("Please input your SQL database password: ")
-    with connect(host="localhost", user="root", password=sql_password, database="common_password_db") as connection:
+    class PasswordChecker():
+        def check_list(self, password):
+            # checks password against passwords in common_passwords.txt. Returns True if password is not in file, False if found.Written by KW
+            # sql_password = getpass("Please input your SQL database password: ")
+            with connect(host="localhost", user="root", password=sql_password, database="common_password_db") as connection:
 
-        with connection.cursor()as cursor:
-            command = f"SELECT * FROM `common_passwords` WHERE password = '{password}';"
-            cursor.execute(command)
-            cursor.fetchall()
-            print(cursor.rowcount)
-            print(password)
-            num_occurences = cursor.rowcount
-            # print("num_occurences assigned")
-            cursor.close()
+                with connection.cursor()as cursor:
+                    command = f"SELECT * FROM `common_passwords` WHERE password = '{password}';"
+                    cursor.execute(command)
+                    cursor.fetchall()
+                    print(cursor.rowcount)
+                    print(password)
+                    num_occurences = cursor.rowcount
+                    # print("num_occurences assigned")
+                    cursor.close()
 
-        if num_occurences > 0:
-            return False
-        elif num_occurences == 0:
-            return True
-
-
-def check_policy(self, password):
-    # reads password policy, checks if password complies with requirements. Returns True if yes, False if not. Written by KW
-    policy_list = self.read_password_policy()
-    # print(policy_list)
-    # now hav ea list defining password policy
-    num_specials = policy_list[0]
-    num_lowercase = policy_list[1]
-    num_uppercase = policy_list[2]
-    num_numbers = policy_list[3]
-    min_length = policy_list[4]
-    max_length = policy_list[5]
-    allowed_specials = policy_list[6]
-
-    count_specials = 0
-    count_lower = 0
-    count_upper = 0
-    count_numbers = 0
-
-    if len(password) < min_length or len(password) > max_length:
-        # not compliant if too short or too long
-        return False
-
-    for letter in password:
-        # check each letter to see if special, lower, upper, or number. Count each of these
-        if letter.isdigit():
-            count_numbers += 1
-        elif letter.isupper():
-            count_upper += 1
-        elif letter.islower():
-            count_lower += 1
-        elif letter in allowed_specials:
-            count_specials += 1
-        else:
-            # return false if part of password is not in any allowed category
-            print("illegal character")
-            return False
-
-    # now have a count of all the lower, upper, special characters and numbers
-    if count_upper >= num_uppercase and count_lower >= num_lowercase and count_specials >= num_specials and count_numbers >= num_numbers:
-        return True
-    else:
-        return False
+                if num_occurences > 0:
+                    return False
+                elif num_occurences == 0:
+                    return True
 
 
-def check_user_details(self, password, user_firstname, user_lastname, user_birthyear):
-    # checks if the password contains the user name or year of birth. Outputs True if no user details in the password. Written by KW
-    if user_firstname in password:
-        return False
-    elif user_lastname in password:
-        return False
-    elif user_birthyear in password:
-        return False
-    else:
-        return True
+        def check_policy(self, password):
+            # reads password policy, checks if password complies with requirements. Returns True if yes, False if not. Written by KW
+            policy_list = self.read_password_policy()
+            # print(policy_list)
+            # now hav ea list defining password policy
+            num_specials = policy_list[0]
+            num_lowercase = policy_list[1]
+            num_uppercase = policy_list[2]
+            num_numbers = policy_list[3]
+            min_length = policy_list[4]
+            max_length = policy_list[5]
+            allowed_specials = policy_list[6]
+
+            count_specials = 0
+            count_lower = 0
+            count_upper = 0
+            count_numbers = 0
+
+            if len(password) < min_length or len(password) > max_length:
+                # not compliant if too short or too long
+                return False
+
+            for letter in password:
+                # check each letter to see if special, lower, upper, or number. Count each of these
+                if letter.isdigit():
+                    count_numbers += 1
+                elif letter.isupper():
+                    count_upper += 1
+                elif letter.islower():
+                    count_lower += 1
+                elif letter in allowed_specials:
+                    count_specials += 1
+                else:
+                    # return false if part of password is not in any allowed category
+                    print("illegal character")
+                    return False
+
+            # now have a count of all the lower, upper, special characters and numbers
+            if count_upper >= num_uppercase and count_lower >= num_lowercase and count_specials >= num_specials and count_numbers >= num_numbers:
+                return True
+            else:
+                return False
+
+
+        def check_user_details(self, password, user_firstname, user_lastname, user_birthyear):
+            # checks if the password contains the user name or year of birth. Outputs True if no user details in the password. Written by KW
+            if user_firstname in password:
+                return False
+            elif user_lastname in password:
+                return False
+            elif user_birthyear in password:
+                return False
+            else:
+                return True
+                
+        def read_password_policy(self):
+        #reads in password policy, returns variables as a list. Written by KW
+        # password_policy = open('password_policy.txt', 'r')
+        policy = configparser.ConfigParser()
+        policy.read('password_policy.txt')
+        num_specials = policy.getint('Policy', 'num_specials')
+        num_lowercase = policy.getint('Policy','num_lowercase')
+        num_uppercase = policy.getint('Policy','num_uppercase')
+        num_numbers = policy.getint('Policy','num_numbers')
+        min_length = policy.getint('Policy','min_length')
+        max_length = policy.getint('Policy','max_length')
+        allowed_specials = policy.get('Policy','allowed_specials')
+
+        return [int(num_specials), int(num_lowercase), int(num_uppercase), int(num_numbers), int(min_length), int(max_length), allowed_specials]
