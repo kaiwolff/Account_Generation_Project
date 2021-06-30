b1d08529c3786991862fd186ce6375a79ebe76c3
TyreeBlakeBailey <tyreeblakebailey@hotmail.com>
2021-06-30 16:16:16 +0100

    Test access right

---
+++ test_cases/test_access_rights.py
@@ -0,0 +1,15 @@
+import pytest
+import unittest
+import configparser
+import string
+import random
+
+
+class TestAcessRights(unittest.TestCase):
+
+    def test_access(self):
+        #change user name
+        #check admin
+        self.assertTrue(self.checker.check_admin("admin","admin")) # Fun Checkadmin takes user,pass from input User login Test admin account
+        self.assertEqual(self.checker.change_access("TestUser"),1) # changing the TestUser account into a admin should return 1 row edited
+        self.assertEqual(self.checker.change_username("TestUser", "NewAdmin"),1)#takes the Username and chnages it to the new username
