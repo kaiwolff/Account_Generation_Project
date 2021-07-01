5ec9c80cea2178e737a08d43ce8edbeeaff7869e
TyreeBlakeBailey <tyreeblakebailey@hotmail.com>
2021-07-01 11:35:46 +0100

    list of funcs added vars

--- Account-Generator/user_account_details.py
+++ Account-Generator/user_account_details.py
@@ -1,17 +1,24 @@
 class UserAccountDetails():
 
-    def check_admin():# check if the admin value is true
+    def check_admin(self, user_name):# check if the admin value is true
         pass
 
-    def change_access(): # changes the value of admin role column
+    def change_access(self, user_name): # changes the value of admin role column
         pass
 
-    def change_username(): # only if the user is an admin, allows to change the user name
+    def change_username(self, old_user_name, new_user_name): # only if the user is an admin, allows to change the user name
 
         pass
 
-    def delete_new_user(): # deletes user details
-        pass
+    def delete_new_user(self, user_name): # deletes user details
+
+        #with connect(host="localhost", user="root", password=sql_password, database="User_database.db") as connection:
+
+              with connection.cursor()as cursor:
+                  command = f"DELETE FROM User_database WHERE UserName= '{user_name}';"
+                  cursor.execute(command)
+                  cursor.close()
+                  pass
 
     def check_existence(self, user_name): # checks if a user exists in a database
 
