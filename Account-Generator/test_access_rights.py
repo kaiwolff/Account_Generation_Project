4fe19489a787169ab8ca13077adcedb85ea88812
TyreeBlakeBailey <tyreeblakebailey@hotmail.com>
2021-07-02 17:25:44 +0100

    checker test access right

--- Account-Generator/test_access_rights.py
+++ Account-Generator/test_access_rights.py
@@ -8,12 +8,11 @@ from user_account_details import UserAccountDetails
 
 class TestAccessRights(unittest.TestCase):
 
+    checker = UserAccountDetails()
+
     def test_access(self):
         #change user name
         #check admin
-
-        checker = UserAccountDetails()
-
         self.assertTrue(self.checker.check_admin("admin","admin")) # Fun Checkadmin takes user,pass from input User login Test admin account
         self.assertEqual(self.checker.change_to_manager("TestUser","admin", "admin"),"The account has been changed to admin status.") # changing the TestUser account into a admin should return 1 row edited
         self.assertEqual(self.checker.change_to_user("TestUser","admin", "admin"),"The account has been changed to user")
