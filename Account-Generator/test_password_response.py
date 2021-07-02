57eeb1324206b8704cd11ebcdc8ec76d249e024f
TyreeBlakeBailey <tyreeblakebailey@hotmail.com>
2021-07-02 14:13:15 +0100

    missed one test file

--- Account-Generator/test_password_response.py
+++ Account-Generator/test_password_response.py
@@ -11,7 +11,7 @@ class ResponseTest(unittest.TestCase):
 	checker = UserAccountDetails()
 
 	def test_check(self):
-		self.assertEqual(self.checker.create_new_user("test_username","test_firstname","test_lastname", "1997", "password"), "Your password is weak.")
-		self.assertEqual(self.checker.create_new_user("test_username","test_firstname","test_lastname", "1997", "s$Y9h70OXO)nXb7Y"), "You have been successfully added to the database system.")
-		self.assertEqual(self.checker.create_new_user( "Afshana", "Afshana", "Begum", "1997", "Afshanapw"), "This password is weak.")
-		self.assertEqual(self.checker.create_new_user("Afshana_username","Afshana", "Begum", "1997", "s$Y9h70OXO)nXb7Y"), "You have been successfully added to the database system.")
+		self.assertEqual(self.checker.create_new_user("test_username","test_firstname","test_lastname", "1997", "password"), "Your password is weak.") # testing for password
+		self.assertEqual(self.checker.create_new_user("test_username","test_firstname","test_lastname", "1997", "s$Y9h70OXO)nXb7Y"), "You have been successfully added to the database system.") # testing for password
+		self.assertEqual(self.checker.create_new_user( "Afshana", "Afshana", "Begum", "1997", "Afshanapw"), "This password is weak.") # test checks is user details are in password
+		self.assertEqual(self.checker.create_new_user("Afshana_username","Afshana", "Begum", "1997", "s$Y9h70OXO)nXb7Y"), "You have been successfully added to the database system.") 
